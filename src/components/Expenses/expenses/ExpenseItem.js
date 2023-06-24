643364314ece8ad0cb88493bc4e93bf14dc133fe
ReguntaSneha <reguntasneha000@gmail.com>
2023-06-24 13:06:07 +0530

    deleting expense

--- src/components/Expenses/expenses/ExpenseItem.js
+++ src/components/Expenses/expenses/ExpenseItem.js
@@ -6,15 +6,20 @@ import ExpenseDate from '../ExpenseDate';
 import './Expenses.css';
 
 const ExpenseItem=(props)=> {
+  const clickHandler = ()=>{
+    console.log('clicked!!!');
+  };
   return (
     <div className='expense-item'>
-
-        <ExpenseDate date ={props.date} />
-
-        <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
-
+      <ExpenseDate date ={props.date} />
+      <ExpenseDetails amount={props.amount}  location={props.location}   title={props.title} />
+      <button onClick={clickHandler}>Delete Expense</button>
     </div>
-  );
+             
+
+  
+  
+ );
 }
 
 export default ExpenseItem;
