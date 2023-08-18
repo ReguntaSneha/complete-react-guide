c7a24a0c6a5130912af6798bb913b5db8e13c9b2
ReguntaSneha <reguntasneha000@gmail.com>
2023-08-18 19:46:54 +0530

    form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,20 +2,32 @@ import React,{useState} from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm=()=> {
-    const [enteredTitle, setEnteredTitle]=useState('');
-    const [enteredAmount, setEnteredAmount]=useState('');
-    const [enteredDate, setEnteredDate]=useState('');
+    const[enteredTitle,setEnteredTitle]=useState('');
+    const[enteredAmount,setEnteredAmount]=useState('');
+    const[enteredDate,setEnteredDate]=useState('');
+     
      const titleChangeHandler=(event)=>{
-        setEnteredTitle(event.target.value);
+        setEnteredTitle(event.target.value);       
      };
      const amountChangeHandler=(event)=>{
-        setEnteredAmount(event.target.value);
+        setEnteredAmount(event.target.value); 
+       
      };
-     const dateChangeHandler=(event)=>{
-        setEnteredDate(event.target.value);
+    const dateChangeHandler=(event)=>{
+        setEnteredDate(event.target.value); 
      };
+    const submitHandler=(event)=>{
+        event.preventDefault();
+      const expenseData={  
+        title:enteredTitle,
+        amount:enteredAmount,
+        date:new Date(enteredDate)
+    };
+    console.log(expenseData);
+    };
+
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='New-expense_control'>
                     <label>Title</label>
